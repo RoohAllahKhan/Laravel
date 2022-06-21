0b706693d64c68aaf0bdb3395533fc904928deeb
RoohAllahKhan <roohallahkhan20@gmail.com>
2022-06-22 00:51:55 +0500

    Database configuration and query

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UsersController extends Controller
 {
     //
     public function loadView()
     {
-        $data = ['rooh', 'khan', 'peter', 'jason'];
-        return view("users", ["user" => "rooh", "users" => $data]);
+//        $data = ['rooh', 'khan', 'peter', 'jason'];
+//        return view("users", ["user" => "rooh", "users" => $data]);
+//        dd(DB::);
+        return DB::select("select * from User");
     }
 
     public function signIn(Request $request)
